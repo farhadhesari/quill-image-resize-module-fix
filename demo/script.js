d6cea1fef485f5693915fa0aab27ac90aa5ed7f2
Farhad Hesari <farhadashrafhessari@gmail.com>
2024-09-26 16:08:22 +0330

    fix attribute related errors, update deprecated dependencies(quill v2)

--- demo/script.js
+++ demo/script.js
@@ -1,6 +1,22 @@
-var quill = new Quill('#editor', {
-	theme: 'snow',
+var quill = new Quill("#editor", {
+	theme: "snow",
 	modules: {
-		imageResize: {}
-	}
+		toolbar: {
+			container: [
+				[{ font: [] }, { size: [] }], // Font and size
+				["bold", "italic", "underline", "strike"], // Bold, italic, underline, strikethrough
+				[{ color: [] }, { background: [] }], // Font color and background color
+				[{ script: "sub" }, { script: "super" }], // Subscript and superscript
+				[{ header: 1 }, { header: 2 }], // Headers (1 and 2)
+				[{ list: "ordered" }, { list: "bullet" }], // Lists (ordered, bullet)
+				[{ indent: "-1" }, { indent: "+1" }], // Indent (outdent, indent)
+				[{ direction: "rtl" }], // Text direction
+				[{ align: [] }], // Text alignment
+				["link", "image", "video"], // Links, images, and videos
+				["clean"], // Remove formatting (clean button)
+				["blockquote", "code-block"], // Blockquote and code block
+			],
+		},
+		imageResize: {},
+	},
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,47 @@
-var path = require('path');
+var path = require("path");
 
 module.exports = {
-    entry: "./src/ImageResize.js",
-    output: {
-        path: __dirname,
-        library: 'ImageResize',
-        libraryTarget: 'umd',
-        filename: "image-resize.min.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                include: path.join(__dirname, 'src'),
-                exclude: /(node_modules|bower_components)/,
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        "presets": [["es2015", { "modules": false }]],
-                        "plugins": ["babel-plugin-transform-class-properties"]
-                    }
-                }]
-            },
-            {
-                test: /\.svg$/,
-                use: [{
-                    loader: 'raw-loader'
-                }]
-            }
-        ]
-    }
+	mode: "development", // Set the mode to 'development' or 'production'
+	entry: "./src/ImageResize.js",
+	output: {
+		path: __dirname,
+		library: "ImageResize",
+		libraryTarget: "umd",
+		filename: "image-resize.min.js",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				include: path.join(__dirname, "src"),
+				exclude: /(node_modules|bower_components)/,
+				use: [
+					{
+						loader: "babel-loader",
+						options: {
+							presets: ["@babel/preset-env"],
+							plugins: [
+								"@babel/plugin-transform-class-properties",
+							],
+						},
+					},
+				],
+			},
+			{
+				test: /\.svg$/,
+				use: [
+					{
+						loader: "raw-loader",
+					},
+				],
+			},
+		],
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, "demo"),
+		},
+		compress: true,
+		port: 9000,
+	},
 };
